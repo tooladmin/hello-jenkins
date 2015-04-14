8a5ab7c87e813f1a1f69431b7b68958c79747e6e
tooladmin <viveksymantec@gmail.com>
2015-04-14 08:41:51 +0530

    Create MyFirstJenkins.java

---
+++ MyFirstJenkins.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.jenkins.demo;
+
+/**
+ * @author Hema
+ *
+ */
+public class MyFirstJenkins {
+
+	/**
+	 * 
+	 */
+	public MyFirstJenkins() {
+
+		System.out.println("Hi \nThis is my first Jenkins Program");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		new MyFirstJenkins();
+	}
+
+}
